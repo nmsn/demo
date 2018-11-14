b476aa087c9de6e47f025feb31a920982c1ed176
shangyuan.ning <shangyuan.ning@manaowan.com>
2018-11-14 16:18:34 +0800

    地图拖拽以及canvas移动

--- Canvas/drag.js
+++ Canvas/drag.js
@@ -1,40 +1,52 @@
-var obj = document.querySelector("span");
-var text = document.querySelector(".text");
+var obj = document.querySelector("#map");
+var container = document.querySelector("#container");
+
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+
+// var text = document.querySelector(".text");
 obj.addEventListener('touchstart', function(event) {
-      event.preventDefault();
+    event.preventDefault();
     // 如果这个元素的位置内只有一个手指的话
     if (event.targetTouches.length == 1) {
     var touch = event.targetTouches[0];
     // 把元素放在手指所在的位置 
-    var disX=touch.pageX-obj.offsetLeft;
-    var disY=touch.pageY-obj.offsetTop;
-    var box=document.querySelector(".box");
-    var width= box.clientWidth-obj.offsetWidth;
-      obj.addEventListener('touchmove',move);
-      function move(event){
-        var touch2=event.targetTouches[0];
-        var l=touch2.pageX-disX;
-        var t=touch2.pageY-disY;
-        if(l<0){
-            l=0;
-        };
-        if(l>width){
-            l=width;
-        };
-        if(t<0){
-            t=0;
-        };
-        if(t>0){
-            t=0;
-        };
-        obj.style.left=l+'px';
-        obj.style.top=t+'px';
-        text.innerHTML = (l/width).toFixed(1);
-    };
-      obj.addEventListener('touchend',chend);
-      function chend(event){
-          obj.removeEventListener('touchmove',move);
-          obj.removeEventListener('touchend',chend);
+    var disX = touch.pageX - obj.offsetLeft; // 点击点和点击物体边距离差值x
+    var disY = touch.pageY - obj.offsetTop; // 点击点和点击物体边距离差值y
+
+    // var box = document.querySelector(".box");
+
+    // var width = box.clientWidth - obj.offsetWidth;
+
+    obj.addEventListener('touchmove',move);
+
+    function move(event){
+      var touch2 = event.targetTouches[0];
+      var left = touch2.pageX - disX;
+      var top = touch2.pageY - disY;
+      var objWidth = obj.offsetWidth;
+      var objHeight = obj.offsetHeight;
+      if(left > 0){
+          left = 0;
       };
+      if(left + objWidth < containerWidth){
+          left = containerWidth - objWidth;
+      };
+      if(top > 0){
+          top = 0;
+      };
+      if(top + objHeight < containerHeight){
+          top = containerHeight - objHeight;
+      };
+      obj.style.left = left + 'px';
+      obj.style.top = top + 'px';
     };
-    }, false);
+
+    obj.addEventListener('touchend',chend);
+
+    function chend(event){
+      obj.removeEventListener('touchmove',move);
+      obj.removeEventListener('touchend',chend);
+    };
+  };
+}, false);
