77cbd715b6ebfb1a9cafad7bfafbdd557844bc09
nmsn <136696700@qq.com>
2020-03-07 23:35:36 +0800

    洋葱模型实现

---
+++ 洋葱模型实现.js
@@ -0,0 +1,24 @@
+function app() {}
+app.routes = [];
+app.use = function(fn) {
+  app.routes.push(fn);
+};
+
+app.use((ctx, next) => {
+  console.log(1);
+  next();
+  console.log(2);
+});
+app.use((ctx, next) => {
+  console.log(3);
+  next();
+  console.log(4);
+});
+
+let index = 0;
+function next() {
+  if (app.routes.length === index) return;
+  let route = app.routes[index++];
+  route({}, next);
+}
+next();
