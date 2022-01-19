15ffe0144892587389e29bc21550b2baf5c3152a
nmsn <136696700@qq.com>
2022-01-19 23:12:27 +0800

    节流补充

--- 实现throttle节流函数.js
+++ 实现throttle节流函数.js
@@ -1,16 +1,32 @@
-// 函数节流的实现;
+// 函数节流的实现
+// 时间戳版
 function throttle(fn, delay) {
   let curTime = Date.now();
 
   return function () {
     let context = this,
-      args = arguments,
+      args = [...arguments],
       nowTime = Date.now();
 
     // 如果两次时间间隔超过了指定时间，则执行函数。
     if (nowTime - curTime >= delay) {
       curTime = Date.now();
       return fn.apply(context, args);
+    }
+  };
+}
+
+// 定时器版
+function throttle(fun, wait) {
+  let timeout = null;
+  return function () {
+    let context = this;
+    let args = [...arguments];
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        fun.apply(context, args);
+        timeout = null;
+      }, wait);
     }
   };
 }
