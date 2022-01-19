263dc4236cc422d857fef1e545ad9efbdf5f5fe0
nmsn <136696700@qq.com>
2022-01-19 23:12:17 +0800

    防抖

--- 实现debounce防抖函数.js
+++ 实现debounce防抖函数.js
@@ -4,7 +4,7 @@ function debounce(fn, wait) {
 
   return function () {
     let context = this,
-      args = arguments;
+      args = [...arguments];
 
     // 如果此时存在定时器的话，则取消之前的定时器重新记时
     if (timer) {
