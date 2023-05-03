7fba157841579c436fc26e4c6aeab644a23ccb66
nmsn <136696700@qq.com>
2023-05-03 13:43:02 +0800

    feat: 更新防抖函数

--- 实现debounce防抖函数.js
+++ 实现debounce防抖函数.js
@@ -1,20 +1,35 @@
-// 函数防抖的实现
-function debounce(fn, wait) {
+function debounce(fn, delay) {
   let timer = null;
-
-  return function () {
-    let context = this,
-      args = [...arguments];
-
-    // 如果此时存在定时器的话，则取消之前的定时器重新记时
+  return function (...args) {
     if (timer) {
-      clearTimeout(timer);
-      timer = null;
+      clearTimeout(timer); //先清除定时器
     }
 
-    // 设置定时器，使事件间隔指定事件后执行
     timer = setTimeout(() => {
-      fn.apply(context, args);
-    }, wait);
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+// 立即执行版
+function debounce(fn, delay, immediate) {
+  let timer = null;
+
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    // timer 后续一直存在，clearTimeout 只是清理了定时器，timer 的值还在
+    if (immediate && !timer) {
+      fn.apply(this, args);
+    } else {
+      timer = setTimeout(() => {
+        // 这一句加了，表示执行过后，再次执行时还会立即执行一次
+        // 不加就是立即执行整个过程中只有一次
+        // timer = null
+        fn.apply(this, args);
+      }, delay);
+    }
   };
 }
