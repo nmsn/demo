7701cc63c395d62aa2628640d9cb6cd306e6ab17
nmsn <136696700@qq.com>
2023-05-09 17:16:39 +0800

    feat: update new

--- new操作符.js
+++ new操作符.js
@@ -17,5 +17,5 @@ function myNew(constructor, ...args) {
   // 执行构造方法, 并为其绑定新this, 这一步是为了让构造方法能进行this.name = name之类的操作, args是构造方法的入参, 因为这里用myNew模拟, 所以入参从myNew传入
   const result = constructor.apply(newObj, args);
   // 如果构造方法已经return了一个对象，那么就返回该对象，否则返回myNew创建的新对象（一般情况下，构造方法不会返回新实例，但使用者可以选择返回新实例来覆盖new创建的对象）
-  return typeof result === "object" ? result : newObj;
+  return typeof result === "object" && result !== null ? result : newObj;
 }
