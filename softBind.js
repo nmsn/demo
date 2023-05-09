17e265021b857042f7228555a8881cd2d77dfd45
nmsn <136696700@qq.com>
2023-05-09 17:26:26 +0800

    feat: add softBind

---
+++ softBind.js
@@ -0,0 +1,9 @@
+Function.prototype.softBind = function (obj, ...rest) {
+  const fn = this;
+  const bound = function (...args) {
+    const o = !this || this === (window || global) ? obj : this;
+    return fn.apply(o, [...rest, ...args]);
+  };
+
+  return bound;
+};
